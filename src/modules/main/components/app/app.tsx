2d806d492efd143df674bde6acd553f32a7d9193
Danil Mirgaev <danil.mirgaev@id.thesoul.io>
2023-02-27 15:23:23 +0400

    feat: Init commit

---
+++ src/modules/main/components/app/app.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+export const App: FC = () => {
+  return (
+    <main>
+      <header>
+        <h1>Color dropper</h1>
+      </header>
+    </main>
+  );
+}
+
+export default App;
